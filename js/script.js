93d2b5828def75861355599546109aa5a0d4968e
anupam17290 <factwithanupam@gmail.com>
2026-06-01 15:59:47 +0530

    Add hover effect to card elements

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 console.log("CatWebs Loaded");
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+
+card.addEventListener('mouseenter', () => {
+card.style.boxShadow =
+'0 10px 25px rgba(255,176,0,0.3)';
+});
+
+card.addEventListener('mouseleave', () => {
+card.style.boxShadow = 'none';
+});
+
+});
